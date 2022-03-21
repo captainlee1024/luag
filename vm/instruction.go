515deb1eb95b57f0ad2739e9d56e02b13d5b5949
litenghui <644052732@qq.com>
2022-03-21 10:55:30 +0800

    code format

--- vm/instruction.go
+++ vm/instruction.go
@@ -2,15 +2,10 @@ package vm
 
 type Instruction uint32
 
-// 从指令中提取操作码
-func (self Instruction) Opcode() int {
-	return int(self & 0x3F)
+// Opcode 从指令中提取操作码
+func (instr Instruction) Opcode() int {
+	return int(instr & 0x3F)
 }
-
-// 从iABC指令提取参数
-// 从iABx指令提取参数
-// 从iAsBx指令提取参数
-// 从iAx指令提取参数
 
 const MAXARG_Bx = 1<<18 - 1       // 262143
 const MAXARG_sBx = MAXARG_Bx >> 1 // 131071
@@ -29,6 +24,7 @@ const MAXARG_sBx = MAXARG_Bx >> 1 // 131071
  31      23      15       7      0
 */
 
+// ABC 从iABC指令提取参数
 func (instr Instruction) ABC() (a, b, c int) {
 	a = int(instr >> 6 & 0xFF)
 	c = int(instr >> 14 & 0x1FF)
@@ -36,33 +32,36 @@ func (instr Instruction) ABC() (a, b, c int) {
 	return
 }
 
-func (self Instruction) ABx() (a, bx int) {
-	a = int(self >> 6 & 0xFF)
-	bx = int(self >> 14)
+// ABx 从iABx指令提取参数
+func (instr Instruction) ABx() (a, bx int) {
+	a = int(instr >> 6 & 0xFF)
+	bx = int(instr >> 14)
 	return
 }
 
-func (self Instruction) AsBx() (a, sbx int) {
-	a, bx := self.ABx()
+// AsBx 从iAsBx指令提取参数
+func (instr Instruction) AsBx() (a, sbx int) {
+	a, bx := instr.ABx()
 	return a, bx - MAXARG_sBx
 }
 
-func (self Instruction) Ax() int {
-	return int(self >> 6)
+// Ax 从iAx指令提取参数
+func (instr Instruction) Ax() int {
+	return int(instr >> 6)
 }
 
-func (self Instruction) OpName() string {
-	return opcodes[self.Opcode()].name
+func (instr Instruction) OpName() string {
+	return opcodes[instr.Opcode()].name
 }
 
-func (self Instruction) OpMode() byte {
-	return opcodes[self.Opcode()].opMode
+func (instr Instruction) OpMode() byte {
+	return opcodes[instr.Opcode()].opMode
 }
 
-func (self Instruction) BMode() byte {
-	return opcodes[self.Opcode()].argBMode
+func (instr Instruction) BMode() byte {
+	return opcodes[instr.Opcode()].argBMode
 }
 
-func (self Instruction) CMode() byte {
-	return opcodes[self.Opcode()].argCMode
+func (instr Instruction) CMode() byte {
+	return opcodes[instr.Opcode()].argCMode
 }
